b44f4effe9d659e4add25d976c7d9b62d9c9391e
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2019-08-20 02:47:09 -0300

    change db

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://upplify:upplify123.mlab.com:11268/upplify', { useNewUrlParser: true });
+mongoose.connect('mongodb://upplify:upplify123@ds211268.mlab.com:11268/upplify', { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
